eb3352e63ed697d8de41b272d26385bf015b8439
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-05-06 23:53:34 +0530

    created addto cartbutton

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-const inputBox = document.querySelector("input-box")
+const inputBox = document.querySelector("#input-box");
 
-const input
+const listContainer = document.querySelector(".list-container");
+
+//function of adding task
+function AddTask() {
+  if (inputBox.value === "") {
+    alert("please add your text");
+  } else {
+    //creating li element
+    let createList = document.createElement("li");
+    createList.classList = "check-box";
+    createList.innerHTML = inputBox.value;
+    listContainer.appendChild(createList);
+    //Creating cross mark button
+    let creatCrossIcon = document.createElement("img");
+    creatCrossIcon.classList("cross-icon");
+    creatCrossIcon.src = "./asset/x-circle-regular-24.png";
+    createList.appendChild(creatCrossIcon);
+  }
+  inputBox.value = "";
+}
